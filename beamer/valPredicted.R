a10a0b293a14df74d83d96197f4b127030e30640
Clément Tamines <clement.tamines@gmail.com>
2016-05-24 22:00:53 +0100

    More explaination and example file

---
+++ beamer/valPredicted.R
@@ -0,0 +1,5 @@
+>ptitanic$survived[1:10]
+survived survived   died   died   died   survived survived died survived died    
+
+> predicted
+survived survived survived died survived   died   survived died survived died 
