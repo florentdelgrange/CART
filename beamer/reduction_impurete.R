7cb3975d8374655c0a5167b1e0da74a075de8dac
radioGiorgio <florent.delgrange@gmail.com>
2016-05-24 18:50:46 +0200

    some minor errors corrected

--- beamer/reduction_impurete.R
+++ beamer/reduction_impurete.R
@@ -1,13 +1,13 @@
-n=length(ptitanic$survived)
-ndL=which(ptitanic$sex=="male")
-indR=which(ptitanic$sex=="female")
-pL=length(indL)/n
-pR=length(indR)/n
-IL=I(ptitanic$survived[indL])
-IR=I(ptitanic$survived[indR])
-pA=1
-IA=calcul_impurete(ptitanic$survived)
-perte_impurete=pA*IA-pL*IL-pR*IR
+IA = I(ptitanic$survived) # impurete principale
+n = length(ptitanic$survived)
+indL = which(ptitanic$sex=="male")
+indR = which(ptitanic$sex=="female")
+pL = length(indL)/n
+pR = length(indR)/n
+IL = I(ptitanic$survived[indL])
+IR = I(ptitanic$survived[indR])
+pA =1
+perte_impurete = pA*IA-pL*IL-pR*IR
 
 > print(perte_impurete)
 [1] 0.1319704
